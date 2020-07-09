e07e94e646347cd72150714a6f53bd3ed75cc2be
RazanAl-Quran <r.alquran@ltuc.com>
2020-07-09 12:17:58 +0300

    class09 done

---
+++ class-09/demo/myStore.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const myHelpers = require('./helpers.js');
+console.log(myHelpers);
+const hours = ['7am', '8am', '9am'];
+
+const Store = function(location, min, max, avg) {
+  this.location = location;
+  this.minCustomersPerHour = min;
+  this.maxCustomersPerHour = max;
+  this.avgCookiesPerCustomer = avg;
+  this.projections = {};
+  this.projectSales();
+  Store.stores.push(this);
+};
+
+Store.stores = [];
+
+Store.prototype.projectSales = function() {
+  hours.forEach( (hour) => {
+    this.projections[hour] = myHelpers.randomNumberBetween(this.minCustomersPerHour, this.maxCustomersPerHour) * this.avgCookiesPerCustomer;
+  });
+  console.log(myHelpers.pet);
+
+};
+
+module.exports = Store;
